2d7a9c08832cba6bb0ab2801f09501c3a7fc7f6f
tadeuasarro <tadeutas@gmail.com>
2021-03-01 17:51:26 -0300

    User can now logout and erase session

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,19 @@
-const Home = () => (
-  <div>This is working</div>
-);
+import { useDispatch } from 'react-redux';
+import { eraseSession } from '../../actions/index';
+
+const Home = () => {
+  const dispatch = useDispatch(state => state);
+
+  const handleClick = () => {
+    document.cookie = 'username=';
+    dispatch(eraseSession());
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleClick()} type="button">Logout</button>
+    </div>
+  );
+};
 
 export default Home;
