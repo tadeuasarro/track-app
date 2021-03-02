e2c8f4bc62454bfb8784d036206857484dd4bed0
tadeuasarro <tadeutas@gmail.com>
2021-03-02 12:58:28 -0300

    Finish home summary for the time being

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,27 @@
 import Footer from '../footer/Footer';
+import './home.css';
 
 const Home = () => (
   <div>
+    <div className="home-container">
+      <div className="home-summary">
+        <div className="home-summary-container">
+          <div className="home-summary-info">1.0 K</div>
+          <p>Expent</p>
+        </div>
+        <div className="home-summary-container">
+          <div className="home-summary-info">2.0 K</div>
+          <p>Target</p>
+        </div>
+        <div className="home-summary-container">
+          <div className="home-summary-info">1.0 K</div>
+          <p>Balance</p>
+        </div>
+      </div>
+      <div className="home-catalogue">
+        this is some content
+      </div>
+    </div>
     <Footer />
   </div>
 );
