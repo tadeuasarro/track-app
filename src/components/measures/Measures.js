88dd6821ca44f0b2a84887effc8edc4a5836821a
tadeuasarro <tadeutas@gmail.com>
2021-03-02 12:18:17 -0300

    Work on the measures screen

--- src/components/measures/Measures.js
+++ src/components/measures/Measures.js
@@ -1,7 +1,17 @@
 import Footer from '../footer/Footer';
+import './measures.css';
 
 const Measures = () => (
   <div>
+    <div className="measures-container">
+      <h3>Create expense</h3>
+      <form className="measures-form">
+        <input className="measures-form-input" placeholder="Category" />
+        <input className="measures-form-input" placeholder="Description" />
+        <input className="measures-form-input" placeholder="Value" />
+        <button className="measures-form-button" type="button">Submit</button>
+      </form>
+    </div>
     <Footer />
   </div>
 );
