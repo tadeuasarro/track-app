29d4cff27c28f2d2bcaf0534eccddfe680650147
tadeuasarro <tadeutas@gmail.com>
2021-03-03 18:42:00 -0300

    Input the username into login works

--- src/api/createSession.js
+++ src/api/createSession.js
@@ -2,7 +2,7 @@ import {
   createSessionPending, createSessionSuccess, createSessionError,
 } from '../actions/session';
 
-const url = 'http://localhost:5000/users';
+const url = 'http://localhost:5000/users/';
 const config = {
   mode: 'cors',
   method: 'GET',
@@ -11,15 +11,16 @@ const config = {
   },
 };
 
-function createSession() {
+function createSession(username) {
   return dispatch => {
     dispatch(createSessionPending());
-    fetch(url, config)
+    fetch(`${url}/${username}`, config)
       .then(res => res.json())
       .then(res => {
         if (res.error) {
           throw (res.error);
         }
+        console.log(res);
         dispatch(createSessionSuccess(res));
         return res;
       })
