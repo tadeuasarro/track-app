31062094e71a61f8a4228fc1a622162d320d47c9
tadeuasarro <tadeutas@gmail.com>
2021-03-03 18:05:10 -0300

    Add redux thunk to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import sessionReducer from './sessionReducer';
 
 const store = createStore(
   combineReducers({
     session: sessionReducer,
   }),
+  applyMiddleware(thunk),
 );
 
 export default store;
