799e2a777a5c39fc8f2877e518528187dc324400
tadeuasarro <tadeutas@gmail.com>
2021-03-06 11:29:41 -0300

    Add create user feature

---
+++ src/api/createUser.js
@@ -0,0 +1,26 @@
+const createExpenditure = async data => {
+  const url = 'http://localhost:5000/users';
+  const config = {
+    mode: 'cors',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  const res = await fetch(url, config);
+  const result = await res.json();
+  if (result === true) {
+    return ({
+      pending: false,
+      error: false,
+    });
+  }
+  return ({
+    pending: false,
+    error: result,
+  });
+};
+
+export default createExpenditure;
--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,17 +1,38 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import createSession from '../../api/createSession';
+import createUser from '../../api/createUser';
 import './login.css';
 
 const Login = () => {
   const dispatch = useDispatch();
+
+  const [state, setState] = useState({
+    pending: false,
+    error: false,
+  });
 
   const handleLoginClick = () => {
     const username = document.getElementById('login-input').value;
     dispatch(createSession(username));
   };
 
-  const handleRegisterClick = () => {
-    console.log('heelo');
+  const handleRegisterClick = async () => {
+    const username = document.getElementById('login-input').value;
+    const target = 0;
+
+    setState({
+      ...state,
+      pending: true,
+    });
+
+    const res = await createUser({ username, target });
+
+    setState(res.state);
+
+    if (!res.error) {
+      dispatch(createSession(username));
+    }
   };
 
   return (
