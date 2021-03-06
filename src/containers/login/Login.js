46842c52f13014a0930162308feafc3c61865ea7
tadeuasarro <tadeutas@gmail.com>
2021-03-06 10:37:38 -0300

    Add register button and fix some css

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -5,16 +5,21 @@ import './login.css';
 const Login = () => {
   const dispatch = useDispatch();
 
-  const handleClick = () => {
+  const handleLoginClick = () => {
     const username = document.getElementById('login-input').value;
     dispatch(createSession(username));
+  };
+
+  const handleRegisterClick = () => {
+    console.log('heelo');
   };
 
   return (
     <div className="login-container">
       <form className="login-form">
         <input id="login-input" className="login-input" type="text" />
-        <button onClick={() => handleClick()} className="login-button" type="button">Login</button>
+        <button onClick={() => handleLoginClick()} className="login-button" type="button">Login</button>
+        <button onClick={() => handleRegisterClick()} className="register-button" type="button">Register</button>
       </form>
     </div>
   );
