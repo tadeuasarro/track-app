c5fa429503fc56e6f311f3b0aa58557ee1f162ea
tadeuasarro <tadeutas@gmail.com>
2021-03-09 19:37:44 -0300

    Finish project

--- src/api/deleteExpenditures.js
+++ src/api/deleteExpenditures.js
@@ -1,6 +1,5 @@
 const deleteExpenditures = async id => {
-  const url = `http://localhost:5000/expenditures/${id}`;
-  // const url = `https://enigmatic-everglades-24941.herokuapp.com/users/${id}`;
+  const url = `https://enigmatic-everglades-24941.herokuapp.com/users/${id}`;
   const config = {
     mode: 'cors',
     method: 'DELETE',
